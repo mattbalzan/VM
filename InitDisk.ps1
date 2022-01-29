f6b5991f1e30a40763694a8a6a2d234d5dde0513
Matt Balzan <loopzilla@gmail.com>
2022-01-29 00:23:53 +0000

    Add files via upload

---
+++ InitDisk.ps1
@@ -0,0 +1,5 @@
+﻿$disks = Get-Disk | Where partitionstyle -eq 'raw' | sort number
+
+$disk | Initialize-Disk -PartitionStyle MBR -PassThru |
+        New-Partition -UseMaximumSize -DriveLetter F |
+        Format-Volume -FileSystem NTFS -NewFileSystemLabel "WSUS" -Confirm:$false -Force
